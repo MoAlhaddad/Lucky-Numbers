2bcf791b6fae229b6b3e307c792ad7640daa58e7
MoAlhaddad <52386724+MoAlhaddad@users.noreply.github.com>
2021-09-03 13:06:52 -0700

    Add files via upload

---
+++ Lucky Number/snake-to-camel.js
@@ -0,0 +1,19 @@
+function snakeToCamel(str) {
+    const camelString = str.replaceAll('__','').split('_').reduce((acc, word, idx) => {
+      if (idx === 0) {
+        return acc + word;
+      }
+      return acc + word.charAt(0).toUpperCase() + word.slice(1);
+    }, '');
+  
+    if (str.startsWith('__') && str.endsWith('__')){
+      return `__${camelString}__`;
+    }
+    if (str.startsWith('__')) {
+      return `__${camelString}`;
+    }
+    if (str.endsWith('__')) {
+      return `${camelString}__`;
+    }
+    return camelString;
+  }
